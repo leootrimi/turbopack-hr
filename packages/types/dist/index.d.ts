8efa19e4bf5c746d528a1f6a86a4e40d19b255ec
Leotrim Halimi <leootrim221@gmail.com>
2026-03-24 23:55:46 +0100

    chore: remove ignored dist files from tracking cache

--- packages/types/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './user/index.js';
-export * from './employee/index.js';
-export * from './team/index.js';
-export * from './time-off/index.js';
-//# sourceMappingURL=index.d.ts.map
--- packages/types/dist/user/index.d.ts
+++
@@ -1,4 +0,0 @@
-export type UserFrame = {
-    id: string;
-};
-//# sourceMappingURL=index.d.ts.map
