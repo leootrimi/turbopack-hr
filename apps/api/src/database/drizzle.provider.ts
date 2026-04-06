98cd40bfe1b9d134732078eaf67df0a1503e2887
Leotrim Halimi <leootrim221@gmail.com>
2026-04-06 22:18:24 +0200

    fixes

--- apps/api/src/database/drizzle.provider.ts
+++ apps/api/src/database/drizzle.provider.ts
@@ -9,11 +9,7 @@ export class DrizzleService {
 
   constructor() {
     this.pool = new Pool({
-      host: process.env.DB_HOST || 'localhost',
-      port: Number(process.env.DB_PORT || 5432),
-      user: process.env.DB_USER || 'admin',
-      password: process.env.DB_PASS || 'admin',
-      database: process.env.DB_NAME! || 'hr',
+      connectionString: process.env.DATABASE_URL!,
     });
 
     this.db = drizzle(this.pool);
--- apps/api/src/database/seed.ts
+++ apps/api/src/database/seed.ts
@@ -32,11 +32,7 @@ const getDateWithOffset = (dayOffset: number, hours = 0, minutes = 0): Date => {
 
 async function seed() {
   const pool = new Pool({
-    host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT || 5432),
-    user: process.env.DB_USER || 'admin',
-    password: process.env.DB_PASS || 'admin',
-    database: process.env.DB_NAME || 'hr',
+    connectionString: process.env.DATABASE_URL!,
   });
 
   const db = drizzle(pool);
