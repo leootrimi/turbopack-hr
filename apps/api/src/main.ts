099966d23038e73647f7532ebcd38018ba6a92e1
Leotrim Halimi <leootrim221@gmail.com>
2026-04-07 17:11:57 +0200

    add cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,16 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['https://turbopack-hr-web.vercel.app', 'https://turbopack-hr-web-skas.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  });
+  app.use(
+    cors({
+      origin: [
+        'https://turbopack-hr-web.vercel.app',
+        'https://turbopack-hr-web-skas.vercel.app'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true
+    })
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
